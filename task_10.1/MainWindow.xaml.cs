7369f3e78282a1bf60ba94cab85a1f1fc5b1c2de
Juliana Haurilava <jrediss.k@gmail.com>
2022-11-13 15:13:44 +0300

    initial commit

---
+++ task_10.1/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using task_5._2;
+
+namespace task_10._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputLineOnClick(object sender, RoutedEventArgs e)
+        {
+            string[] arrayOfWords = Sentence.ToArray(textBox.Text);
+            listBox.ItemsSource = arrayOfWords;
+        }
+
+        private void ReverceLineOnClick(object sender, RoutedEventArgs e)
+        {
+            secondTextBlock.Text = Sentence.ReverseSentence(secondTextBox.Text);
+        }
+    }
+}
---
+++ task_10.1/task_5.2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace task_5._2
+{
+    public class Sentence
+    {
+        /// <summary>
+        /// Создает из строки массив слов
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns>Массив из слов предложения</returns>
+        public static string[] ToArray(string str)
+        {
+            return str.Split(' ');
+        }
+
+        /// <summary>
+        /// Меняет порядок слов в предложении на обратный
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns>Предложение с обратным порядком слов</returns>
+        public static string ReverseSentence(string inputString)
+        {
+            string[] arrayOfWords = ToArray(inputString);
+            Array.Reverse(arrayOfWords);
+            return String.Join(' ', arrayOfWords);
+        }
+    }
+}
